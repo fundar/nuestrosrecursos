85ecee42c8fbcef7a57de2bf32cb768ff2b10fea
Kevin Martínez <n4utilius@gmail.com>
2013-05-06 15:37:27 -0500

    mas secciones

--- wp-content/themes/recursos-theme/index.php
+++ wp-content/themes/recursos-theme/index.php
@@ -6,6 +6,12 @@
   <link rel="stylesheet" href="<?php bloginfo('template_url');?>/sources/css/base.css">
   <link rel="stylesheet" href="<?php bloginfo('template_url');?>/sources/css/curtain.css">
   <link rel="stylesheet" href="<?php bloginfo('template_url');?>/css/home.css">
+  
+  <link href='http://api.tiles.mapbox.com/mapbox.js/v0.6.7/mapbox.css' rel='stylesheet' />
+  <style>
+    body { margin:0; padding:0; }
+    #map { position:absolute; top:0; bottom:0; width:100%; }
+  </style>
 </head>
 <body>
 
@@ -52,85 +58,10 @@
   </li>
 
   <li id="infografia" class="section cover">
+    <h1>Introducción a las Industrias Estractivas en México</h1>
     <div id="brief"></div>
     <div id="info">
-      <div id="texto">
-        En México, los hidrocarburos y otros minerales del subsuelo -como el oro, la plata, el cobre, el
-        hierro, el carbón y muchos más-, pertenecen en dominio a la Nación, es decir, a la población
-        del país. Debido a que el Estado ejerce un monopolio sobre la explotación de hidrocarburos,
-        existen dos r regímenes de explotación distintos para estos bienes no renovables: la paraestatal,
-        Petróleos Mexicanos, detenta el monopolio de la explotación del petróleo y el gas; por otra
-        parte, particulares explotan el resto de los minerales del subsuelo a partir de concesiones que les
-        extiende el Estado. En teoría, la explotación de estos recursos sirve al interés público (esto es, nos
-        beneficia a todas y todos). Sin embargo, las decisiones que se toman con respecto a qué y cuántos
-        recursos explotar (o, en dado caso, si es preferible no llevar a cabo explotación alguna) rara vez
-        toman en cuenta los intereses de las y los habitantes de este país.
-
-        La explotación de los hidrocarburos, se realiza de manera exclusiva por Petróleos Mexicanos
-        (Pemex), entidad paraestatal, es decir, pública. Para llevar a cabo sus actividades, Pemex puede
-        realizar contrataciones de servicios con empresas privadas. En este caso, las empresas realizan
-        trabajos concretos para Pemex quien, al menos en teoría, mantiene el control de los proyectos.
-
-        En cambio, la explotación de aquellos minerales que no son hidrocarburos (salvo algunas
-        excepciones, como es el caso de los elementos radioactivos, explotados exclusivamente por el
-        Estado)- la realizan empresas privadas, muchas de ellas extranjeras. Para ello, el Estado, a través
-        de la Secretaría de Economía, entrega concesiones para la exploración, explotación y extracción
-        del beneficio del mineral (separación del mineral buscado del resto del suelo extraído).
-
-        Toda explotación de recursos conlleva un costo. En toda decisión sobre cuándo, cómo y cuánto
-        se debe explotar se deben tomar en cuenta los costos implícitos de retirar estos bienes de sus
-        yacimientos. Como ya mencionamos arriba, la Nación es propietaria de todos los recursos del
-        subsuelo. Por tanto, es la Nación quien debería poner en la balanza si los beneficios de explotar
-        ciertos yacimientos minerales justifican los costos que su explotación tendrá sobre la sociedad (la
-        misma Nación), el medio ambiente o inclusive las finanzas públicas. Para esto, se debe generar y
-        difundir información suficiente sobre cada proyecto de explotación para que esta decisión pueda
-        tomarse en forma democrática.
-
-        El marco legal vigente que establece mecanismos para prevenir estos costos es muy débil, provee
-        poca información a la sociedad sobre los costos que asumiremos por llevar a cabo cada proyecto
-        de explotación y no queda muy claro cómo se repartirán los beneficios. Lo más grave de todo es
-        que no generan canales a partir de los cuáles la sociedad pueda tomar decisiones democráticas
-        sobre si explotar o no un yacimiento mineral determinado, qué tanto explotarlo, cómo mitigar los
-        daños provocados por la explotación y cómo llevar a cabo una repartición justa de los beneficios
-        de haberlo explotado.
-
-        Existen importantes deficiencias en N, así como (tal como lo establece el Convenio 169
-        de la Organización Internacinoal del Trabajo, que en nuestro país es una norma de rango
-        constitucional)apenas se erigen como un mecanismo de protección ambiental que adolece de
-
-        múltiples deficiencias, entre las cuales figuran prominentemente el hecho de que no toman en
-        cuenta la participación de la sociedad y que este mecanismo no proporciona información en
-        forma oportuna y adecuada a las poblaciones en riesgo de sufrir una afectación grave como
-        consecuencia de proyecto extractivo. estos procedimientos representanL, adscritas a la Secretaría
-        de Medio Ambiente y Recursos Naturales,Si los PEIA presentan múltiples deficiencias, México
-        se encuentra aún más atrasado con respecto a la medición de impactos sociales, puesto que no
-        existe mecanismo alguno que los mida o prevenga. Llos generados por las actividades extractivas
-        tiendan a dar pie a gravesosa
-
-        Tras haber realizado un breve repaso a los graves costos asociados a estas actividades y las
-        deficiencias legales e institucionales existentes para atenderlos, es necesario echar un vistazo a lo
-        que sucede con las ganancias monetarias generadas por la explotación de los hidrocarburos y la
-        riqueza minera.
-
-        Estos dos tipos de regímenes de explotación están sujetos a obligaciones fiscales diferenciadas. En
-        el caso del petróleo, Pemex paga múltiples y cuantiosos derechos, impuestos y aprovechamientos.
-        Entre ellos, el más importante por su cuantía es el Derecho Ordinario sobre Hidrocarburos, el
-        cual supone que Pemex debe pagar el 71.5% del valor de la producción anual -tras la deducción
-        de algunos de sus costos. Debido a ello, y a la baja recaudación tributaria del Estado mexicano,
-        durante los últimos 10 años la explotación del petróleo ha supuesto, en promedio, el 35% de los
-        ingresos del sector público presupuestario.
-
-        En esta materia, la minería presenta un panorama muy diferente. Además de los impuestos que
-        deben pagar todas las empresas, como el IVA y el ISR, las mineras sólo están obligadas a pagar un
-        Derecho por el uso o aprovechamiento de bienes del dominio público -de acuerdo con el número
-        de hectáreas y los años transcurridos desde el otorgamiento de la concesión. Dos concesiones,
-        una sin estar siendo explotada y otra produciendo toneladas de oro, pagan exactamente la
-        misma cantidad por concepto de este derecho. El Estado no le cobra a las empresas mineras
-        ningún derecho (o regalías) en función del valor de su producción. Para el año 2011, el monto
-        de recursos recaudados por este derecho solamente ascendió al 0.78% del total del valor de la
-        producción minera nacional. Es evidente que la Nación mexicana no está recibiendo la parte que
-        le corresponde de las ganancias de la minería, como propietaria en dominio de estos recursos, en
-        tanto que las empresas mineras, bajo el régimen actual, acumulan ganancias millonarias.
+      <div id="texto">s
       </div>
     </div>
   </li>
@@ -185,7 +116,158 @@
       <li></li>
     </ul>
     <div class="line"></div>
+  </li>
 
+  <li id="marcolegal" class="section cover">      
+    <h1>Marco Legal</h1>
+
+    <div id="marco-constitucion" class="marco">
+      <h2> Constitución Política</h2>
+    </div>
+
+    <div id="marco-petrolera" class="marco">
+      <h2> Industria Petrolera </h2>
+    </div>
+
+    <div id="marco-minera" class="marco"> 
+      <h2>Industria Minera</h2>
+    </div>
+
+    <div id="marco-comun" class="marco">
+      <h2> Marco Común Legal </h2>
+    </div>
+
+    <div id="marco-propuestas" class="marco">
+      <h2> Propuestas de Reforma </h2>
+    </div>
+  </li>
+
+  <li id="ingresos" class="section cover">
+    <h1>Ingresos Públicos</h1>
+    <div id="text-contr">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </div>
+    <div id="grafica-contr"></div>
+
+    <!--div id="otros-datos"-->
+      <h2>Otros datos de Interes</h2>
+      <ul id="hidrocarburos" class="ing-ind">
+        <h2>Hidrocarburos</h2>
+        <li> 
+          <img src="" class="min-graf">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.
+            <p>  Download. 
+              <a href="#pdf"> PDF </a> - 
+              <a href="#excel"> Excel</a> 
+            </p>
+          </p> 
+        </li>
+      </ul>
+      <ul id="mineria" class="ing-ind">
+        <h2>Mineria</h2>
+        <li> 
+          <img src="" class="min-graf">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. 
+            <p>  Download. 
+              <a href="#pdf"> PDF </a> - 
+              <a href="#excel" Excel></a>
+            </p>
+          </p> 
+        </li>
+      </ul>
+    <!--/div-->
+
+    <div id="ingr-propuestas" class="marco">
+      <h2> Nuestras Propuestas </h2>
+    </div>
+  </li>
+
+  <li id="mapainteractivo" class="section cover">
+    <h1>Mapa Interactivo</h1>
+    <div id='map'></div>
+  </li>
+
+  <li id="transparencia" class="section cover">
+    <h1>Transparencia y acceso</h1>
+    <div id="left">
+      <h2>Texto simulado</h2>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </div>
+    <ul id="right">
+      <li>
+        <h2>Sector Petrolero</h2>
+      </li>
+      <li>
+        <h2>Sector Minero</h2>
+      </li>
+      <li>
+        <h2>Política Social y ambiental</h2>
+      </li>
+    </ul>
+  </li>
+
+  <li id="publicaciones" class="section cover">
+    <h1>Publicaciones y Multimedia</h1>
+  </li>
+
+  <li id="acerca" class="section cover"> 
+    <h1>Sobre nuestros recursos</h1>
+
+    <h2>Texto fundar</h2>
+    <div class="cuad ab-txt">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </div>
+
+    <h2>Nuestros Recursos</h2>
+    <div class="cuad ab-txt">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </div>
+    
     <div id="footer">
       <ol id="col1">
         <li>Quines somos</li>
@@ -239,39 +321,15 @@
         <span> <b>lugar: </b> México, D.F </span>
         <span> <b>Temática: </b> Transparencia en contratos </span>
       </li>
-    </ul-->
+    </ul>
    
-  </li>
-    <!--div id="footer">
-      <ol id="col1">
-        <li>Quines somos</li>
-        <li>Contacto</li>
-        <li>Fundar, Centro de Análisis e Investigación</li>
-      </ol>
-
-      <ol id="col2">
-        <li>Nuestros Recursos</li>
-        <li>Mapa Interactivo</li>
-
-        <li>Impactos</li>
-        <li>Transparencia</li>
-
-        <li>Marco Legal</li>
-        <li>Publicaciones</li>
-
-        <li>Ingresos</li>
-        <li>Sobre Nosotros</li>
-      </ol>
-
-      <ol id="col3">
-        <img src="<?php bloginfo('template_url');?>/img/fundar.png" id="logo">
-      </ol>
-    </div--> 
-
-
+  </li-->
 </ol>
-
 <script src="<?php bloginfo('template_url');?>/sources/js/libs/jquery.min.js"></script>
+ 
+<script src='http://api.tiles.mapbox.com/mapbox.js/v0.6.7/mapbox.js'></script>
+
+<script>  mapbox.auto('map', 'fundarmexico.map-56rcfk4m');</script>
 <!--script src="<?php bloginfo('template_url');?>/sources/js/libs/curtain.js"></script-->
 <!--script src="<?php bloginfo('template_url');?>/sources/js/home.js"></script-->
 
