aba130b0c474cd6fca5bfcf3136e799b0cbc27de
Carlos Hugo (Fundar) <caarloshugo@gmail.com>
2013-11-19 12:36:32 -0600

    add footer

--- wp-content/themes/recursos-theme/index.php
+++ wp-content/themes/recursos-theme/index.php
@@ -404,3 +404,5 @@
       cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
       proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
     </div>
+    
+<?php get_footer(); ?>
