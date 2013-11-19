c5a544da9b08af1798458c5f7984fed1c9dd8ee2
Carlos Hugo (Fundar) <caarloshugo@gmail.com>
2013-11-19 13:40:38 -0600

    header

--- wp-content/themes/recursos-theme/header.php
+++ wp-content/themes/recursos-theme/header.php
@@ -11,24 +11,26 @@
 
 		<link href='http://api.tiles.mapbox.com/mapbox.js/v0.6.7/mapbox.css' rel='stylesheet' />
 		<style>
-		body { margin:0; padding:0; }
-		#map { position:absolute; top:0; bottom:0; width:100%; }
+			body { margin:0; padding:0; }
+			#map { position:absolute; top:0; bottom:0; width:100%; }
 		</style>
 	</head>
 	<body>
-		<ul class="menu">
-			<li><a href="#inicio" id="_inicio" class="curtain-links normal">Inicio</a></li>
-			<li><a href="#infografia" id="_infografia" class="curtain-links normal">Infografía</a></li>
-			<li><a href="#afectaciones" id="_afectaciones" class="curtain-links normal">Afectaciones</a></li>
-			<li><a href="#marcolegal" id="_marcolegal" class="curtain-links normal">Marco Legal</a></li>
-			<li><a href="#ingresos" id="_ingresos" class="curtain-links normal">Ingresos</a></li>
-			<li><a href="#mapainteractivo" id="_mapainteractivo" class="curtain-links normal">Mapa Interáctivo</a></li>
-			<li><a href="#transparencia" id="_transparencia" class="curtain-links normal">Transparencia</a></li>
-			<li><a href="#publicaciones" id="_publicaciones" class="curtain-links normal">Publicaciones</a></li>
-			<li><a href="#acerca" id="_acerca" class="curtain-links normal">Sobre Nosotros</a></li>
+		<div class="header">
+			<ul class="menu">
+				<li><a href="#inicio" id="_inicio" class="curtain-links normal">Inicio</a></li>
+				<li><a href="#infografia" id="_infografia" class="curtain-links normal">Infografía</a></li>
+				<li><a href="#afectaciones" id="_afectaciones" class="curtain-links normal">Afectaciones</a></li>
+				<li><a href="#marcolegal" id="_marcolegal" class="curtain-links normal">Marco Legal</a></li>
+				<li><a href="#ingresos" id="_ingresos" class="curtain-links normal">Ingresos</a></li>
+				<li><a href="#mapainteractivo" id="_mapainteractivo" class="curtain-links normal">Mapa Interáctivo</a></li>
+				<li><a href="#transparencia" id="_transparencia" class="curtain-links normal">Transparencia</a></li>
+				<li><a href="#publicaciones" id="_publicaciones" class="curtain-links normal">Publicaciones</a></li>
+				<li><a href="#acerca" id="_acerca" class="curtain-links normal">Sobre Nosotros</a></li>
 
-			<!--li><a href="#indpetrolera" id="_indpetrolera" class="curtain-links normal">Industria Petrolera</a></li-->
-		</ul>
+				<!--li><a href="#indpetrolera" id="_indpetrolera" class="curtain-links normal">Industria Petrolera</a></li-->
+			</ul>
+		</div>
 		
 		<ul class="nav_controls">
 			<li><a id="home" href="#inicio" class="curtain-links normal">Top</a></li>
