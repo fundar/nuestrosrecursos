9284c84b3a7a7d33d4d8b9285c1e74b8d3b1d72c
Carlos Hugo (Fundar) <caarloshugo@gmail.com>
2013-11-19 12:23:43 -0600

    header

--- wp-content/themes/recursos-theme/index.php
+++ wp-content/themes/recursos-theme/index.php
@@ -1,42 +1,4 @@
-<!DOCTYPE HTML>
-<html lang="es">
-<head>
-  <meta charset="UTF-8">
-  <title>Nuestros Recursos - Inicio</title>
-  <link rel="stylesheet" href="<?php bloginfo('template_url');?>/sources/css/base.css">
-  <link rel="stylesheet" href="<?php bloginfo('template_url');?>/sources/css/curtain.css">
-  <link rel="stylesheet" href="<?php bloginfo('template_url');?>/css/home.css">
-  <link rel="stylesheet" href="<?php bloginfo('template_url');?>/css/agile_carousel.css">
-  <link rel="stylesheet" href="<?php bloginfo('template_url');?>/css/slider-vertical.css">
-  
-  <link href='http://api.tiles.mapbox.com/mapbox.js/v0.6.7/mapbox.css' rel='stylesheet' />
-  <style>
-    body { margin:0; padding:0; }
-    #map { position:absolute; top:0; bottom:0; width:100%; }
-  </style>
-</head>
-<body>
-
-<ul class="menu">
-  <li> <a href="#inicio"  id="_inicio" class="curtain-links normal"> Inicio </a> </li>
-  <li> <a href="#infografia"  id="_infografia" class="curtain-links normal"> Infografía </a> </li>
-  <li> <a href="#afectaciones"  id="_afectaciones" class="curtain-links normal"> Afectaciones </a> </li>
-
-  <li> <a href="#marcolegal"  id="_marcolegal" class="curtain-links normal"> Marco Legal </a></li>
-  <li> <a href="#ingresos"  id="_ingresos" class="curtain-links normal"> Ingresos </a></li>
-  <li> <a href="#mapainteractivo"  id="_mapainteractivo" class="curtain-links normal"> Mapa Interáctivo</a></li>
-  <li> <a href="#transparencia"  id="_transparencia" class="curtain-links normal"> Transparencia</a></li>
-  <li> <a href="#publicaciones"  id="_publicaciones" class="curtain-links normal"> Publicaciones</a></li>
-  <li> <a href="#acerca"  id="_acerca" class="curtain-links normal"> Sobre Nosotros</a></li>
-
-  <!--li> <a href="#indpetrolera"  id="_indpetrolera" class="curtain-links normal"> Industria Petrolera  </a></li-->
-</ul>
-
-<ul class="nav_controls">
-  <li> <a id="home" href="#inicio" class="curtain-links normal"> Top </a> </li>
-  <li> <a id="up" href="#up" class="curtain-links normal"> ▲  </a> </li>
-  <li> <a id="down" href="#down" class="curtain-links normal"> ▼ </a> </li>
-</ul>
+<?php get_header(); ?>
 
 <ol class="curtains">
   <li id="inicio" class="section" >
