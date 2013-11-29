6d8f4563b4e8a2cfe0baaac7496e47480c9b0757
Carlos Hugo (Fundar) <caarloshugo@gmail.com>
2013-11-29 16:47:46 -0600

    afectaiones

--- wp-content/themes/recursos-theme/index.php
+++ wp-content/themes/recursos-theme/index.php
@@ -76,6 +76,10 @@
 					<br/><br/>
 					
 					En México, buena parte de los estados petroleros tienen índices altos y muy altos de marginación, pese a la riqueza petrolera que día a día se genera en su territorio. Sin bien los problemas de desarrollo de estas regiones no proceden únicamente de la industria petrolera, la actividad extractiva ha agudizado los problemas de marginación y exclusión social. Ello se debe, principalmente, a la afectación de las actividades productivas tradicionales, los daños al medio ambiente, la contaminación y el incremento de los precios que la actividad petrolera ha traído consigo.
+					
+					<br/><br/>
+					
+					A todo ello se une que los recursos que el petróleo genera y que ingresan al erario público tienen poco impacto en las comunidades que se ven afectadas por los proyectos petroleros. En muchas de ellas, no existen los servicios públicos mínimos necesarios para asegurar un nivel de vida adecuado a sus habitantes. Así, no cuentan con escuelas ni centros de salud, las vías de acceso están en malas condiciones, no existe canalización de agua potable, electricidad, etc. De esta manera, la actividad petrolera no tiene un impacto directo en la calidad de vida de una buena parte de la población que habita las regiones petroleras, quien, por el contrario, padece las secuelas de dicha actividad. Por lo tanto, se hace urgente que el Estado implemente políticas públicas adecuadas para hacer frente a esta problemática y asegurar el bienestar y el desarrollo adecuado a la población que se encuentra en las regiones petroleras. 
 				</div>
 				<div id="desc-bar">
 					<div id="desc-back"></div>
